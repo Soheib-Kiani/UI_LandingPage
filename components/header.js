b82ef27580893d3eacf3523667439717a73d36fd
soheib kiani <mrsoheibkiani@gmail.com>
2023-03-03 21:20:11 +0330

    Start Project

---
+++ components/header.js
@@ -0,0 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
+const header = () => {
+  return (
+    <header className="w-full h-12 bg-red-500">
+      <ul className="flex justify-between items-center p-1">
+        <li><img src="headericon"alt ='icons' /></li>
+        <li>tems</li>
+      </ul>
+    </header>
+  );
+};
+
+export default header;
