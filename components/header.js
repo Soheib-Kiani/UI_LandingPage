e21471f6e228d7e714b3ca163b1666ca5b8eb991
soheib kiani <mrsoheibkiani@gmail.com>
2023-03-03 23:57:35 +0330

    Header Component a && Add Font

--- components/header.js
+++ components/header.js
@@ -1,10 +1,24 @@
 /* eslint-disable @next/next/no-img-element */
 const header = () => {
   return (
-    <header className="w-full h-12 bg-red-500">
-      <ul className="flex justify-between items-center p-1">
-        <li><img src="headericon"alt ='icons' /></li>
-        <li>tems</li>
+    <header className="max-w-7xl mx-auto  h-16 ">
+      <ul className="flex justify-between items-center p-3 mx-3">
+          {/* <img src="/icons/headericon.png" alt="icons" /> */}
+          <li className="h-9 w-9 rounded-full">
+            <img src="/favicon.ico" alt="logo" />
+          </li>
+        <div className="flex capitalize text-sm flex-row-reverse items-center gap-x-8">
+          <li className="h-9 w-9 rounded-full">
+            <img src="/icons/profile.png" alt="profile" />
+          </li>
+          <li className="h-4 w-4 rounded-full">
+            <img src="/icons/searchicon.png" alt="search" />
+          </li>
+          <li>contact</li>
+          <li>blog</li>
+          <li>about</li>
+          <li className="text-[#9166F0] font-bold">home</li>
+        </div>
       </ul>
     </header>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import Header from '../../components/header';
 export default function Home() {
   return (
-    <>
+    <div className='font-MonserratFont'>
       <Header />
-    </>
+    </div>
   );
 }
